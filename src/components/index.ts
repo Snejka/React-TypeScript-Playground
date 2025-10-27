9be8aee7c1b06f0c7d2a9d53a2cb887d6b274adf
Snejka <snezhana.p.plachkova@gmail.com>
2025-10-27 16:45:25 +0100

    cleanup

--- src/components/index.ts
+++
@@ -1,9 +0,0 @@
-// Barrel export for all components
-export * from './UI';
-export * from './Navigation';
-export * from './Sessions';
-
-// Main component exports
-export { default as MainHeader } from './Navigation';
-export { default as SessionsList } from './Sessions';
-export { Button, Input, Modal } from './UI';
